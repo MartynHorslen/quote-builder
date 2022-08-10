0ca2590aca9c3d4273746780f9576ab1f2500e3c
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-08-10 17:24:15 +0100

    Refactoring sidebar and navigation. Working up breakpoints but issues if you don't close sidebar while changing view width

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,41 +1,43 @@
 <script>
-import { ref } from 'vue';
+import Navigation from './components/Navigation.vue'
 
 export default {
+  components: {
+    'navigation': Navigation
+  },
+
   data() {
     return {
-      isActive: false
+      sidebar: false
     }
   },
 
-  computed: {
-    console: () => console,
-  },
-
   methods: {
-    active(){
-      this.isActive = ! this.isActive
-    } 
+    sidebarActive() {
+      this.sidebar = !this.sidebar;
+    }
   }
 }
 </script>
 
 <template>
-<nav class="navbar navbar-expand-md navbar-light">
-  <div class="container-fluid justify-content-start">
-    <button class="navbar-toggler hamburger hamburger--spin" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" :class="{'is-active': isActive}" @click="active">
-      <span class="hamburger-box">
-        <span class="hamburger-inner"></span>
-      </span>
-    </button>
-    <a class="navbar-brand ms-3" href="#">Navbar</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-md-auto my-2 my-md-0 mb-md-0 gap-3 me-md-3">
-            <li><router-link to="/">Go to Home</router-link></li>
-            <li><router-link to="/about">Go to About</router-link></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+  <div :class="{sidebarActive: this.sidebar}">
+    <navigation @sidebar="sidebarActive"></navigation>
     <router-view></router-view>
+  </div>
 </template>
+
+<style>
+  .sidebarActive {
+    margin-left: 60%;
+    overflow-x: hidden;
+  }
+  body {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+  }
+  #app {
+    height: 100%;
+  }
+</style>
---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,94 @@
+<script>
+export default {
+  data() {
+    return {
+      isActive: false
+    }
+  },
+
+  computed: {
+    console: () => console,
+  },
+
+  methods: {
+    active(){
+      this.isActive = ! this.isActive
+    } 
+  }
+}
+</script>
+
+<template>
+<nav class="">
+  <div class="container-fluid py-md-2 justify-content-start justify-content-md-between d-md-flex flex-md-row">
+    <button class="hamburger hamburger--spin d-md-none" type="button" :class="{'is-active': isActive}" @click="$emit('sidebar'), active()">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+    <a class="ms-3" href="#">Company Logo</a>
+    <div class="flex-grow-0" :class="isActive ? 'fixedSidebar' : 'hideSidebar'">
+      <h2 class="menu d-md-none">Menu</h2>
+      <ul class="ms-md-auto my-2 my-md-0 mb-md-0 gap-3 me-md-3 d-md-flex list-unstyled ">
+            <li><router-link to="/">Go to Home</router-link></li>
+            <li><router-link to="/about">Go to About</router-link></li>
+            <li>Another link</li>
+            <li>Another link</li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style>
+  .hideSidebar {
+    display: none;
+  }
+  .fixedSidebar {
+    position: fixed;
+    left: 0;
+    top: 0;
+    background-color: #333;
+    height: 100%;
+    width: 60%;
+  }
+  .fixedSidebar ul {
+    padding: 0;
+  }
+  .fixedSidebar li {
+    border-bottom: 1px solid lightgray;
+    padding: 10px 15px;
+  }
+  .fixedSidebar li:first-child {
+    border-top: 1px solid lightgray;
+  }
+  .fixedSidebar li, li a {
+    color: white;
+    list-style: none;
+    text-decoration: none;
+  }
+  .fixedSidebar li {
+    cursor: pointer;
+  }
+  .fixedSidebar li:hover, li:hover a {
+    color: darkgray;
+    background-color: rgb(102, 101, 101);
+  }
+  .fixedSidebar .menu {
+    color: lightgrey;
+    text-align: center;
+    padding: 10px 0;
+  }
+
+  @media (min-width: 768px){
+    .hideSidebar {
+      display: block;
+    }
+    .fixedSidebar {
+      position: inline-block;
+    }
+    .fixedSidebar li, li a {
+    color: black;
+  }
+  }
+</style>
